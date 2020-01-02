2008ebec25b5d1fdef2087748ca5e0099b44033e
Unknown <kramer@consider-it.de>
2020-01-02 19:07:38 +0100

    removing config from .gitignore
    The file no longer contains sensitive information. All data has been transferred .env.

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php return [
+    'client_id' => getenv('CRONNIT_CLIENT_ID'),
+    'client_secret' =>  getenv('CRONNIT_CLIENT_SECRET'),
+    'dbdsn' => getenv('CRONNIT_DBDSN'),
+    'dbuser' => getenv('CRONNIT_DBUSER'),
+    'dbpass' => getenv('CRONNIT_DBPASS'),
+    'url' => getenv('CRONNIT_URL')
+];
