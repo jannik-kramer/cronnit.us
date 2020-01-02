a41d68ffb8a235e917b818d7a8af2d8d133e222c
Unknown <kramer@consider-it.de>
2020-01-02 19:04:24 +0100

    testing without dotenv

--- public_html/index.php
+++ public_html/index.php
@@ -1,10 +1,6 @@
 <?php
 
 require_once __DIR__ . "/../vendor/autoload.php";
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
 require_once __DIR__ . "/../Cronnit.php";
 
 Cronnit::serve();
